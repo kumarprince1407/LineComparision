4c2642962faaaf40494b2e9a3c65bf039fcb965c
Prince Kumar <kumarprince1407@gmail.com>
2023-06-20 23:37:23 +0530

    Initial commit

---
+++ LineComparision/src/com/bridgelabz/linecomparision/LineComparision.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linecomparision;
+import java.util.*;
+public class LineComparision {
+    public static void main(String args[]){
+        System.out.println("Welcome to Line Comparision Problem program.");
+
+    }
+}
