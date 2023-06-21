4660803344eb06cb536eecc6bd171e987588701d
Prince Kumar <kumarprince1407@gmail.com>
2023-06-21 19:31:21 +0530

    Added Use Case 1

--- LineComparision/src/com/bridgelabz/linecomparision/LineComparision.java
+++ LineComparision/src/com/bridgelabz/linecomparision/LineComparision.java
@@ -3,6 +3,20 @@ import java.util.*;
 public class LineComparision {
     public static void main(String args[]){
         System.out.println("Welcome to Line Comparision Problem program.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of x1:");
+        double x1 = sc.nextDouble();
+        System.out.print("Enter the value of x2:");
+        double x2 = sc.nextDouble();
+        System.out.print("Enter the value of y1:");
+        double y1 = sc.nextDouble();
+        System.out.print("Enter the value of y2:");
+        double y2 = sc.nextDouble();
+
+        System.out.println("The length of the line is: "+ (Math.sqrt(Math.pow((x2 - x1),2) )+ Math.pow((y2 -y1),2)));
 
     }
+    /*
+
+     */
 }
